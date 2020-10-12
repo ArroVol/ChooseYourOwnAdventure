2d229e2956de4a307cb7cd5ad8ee22b7b698b853
Aaron <arrovol47@gmail.com>
2020-10-12 16:34:38 -0500

    Second commit

--- src/main/java/edu/ben/cmsc4375/MainApplication.java
+++ src/main/java/edu/ben/cmsc4375/MainApplication.java
@@ -1,8 +1,36 @@
 package edu.ben.cmsc4375;
 
-public class MainApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+import java.awt.*;
+
+public class MainApplication extends Application {
+
+    Button button;
     public static void main(String[] args) {
+        launch(args);
         System.out.println("Choose Wisely");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Choose Your Adventure LIAM");
+       Button button = new Button();
+       button.setLabel("Click");
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root,400,400);
+        scene.getStylesheets();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+//        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+//        layout.getChildren().add(button);
+//    primaryStage
+        System.out.println("LIAM LEVELS UP!");
     }
 }
